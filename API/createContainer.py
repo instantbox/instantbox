184ebca92b2b62163c7aa4c15d7a28aed993662c
import-yuefeng <zhuyuefeng0@outlook.com>
2018-12-19 00:42:22 +0800

    Add API rmContainer & createContainer.

---
+++ API/createContainer.py
@@ -0,0 +1,49 @@
+import docker
+
+class CreateContainer(object):
+
+
+
+    def __init__(self):
+
+        self.client = docker.from_env()
+
+
+    def is_create_container(self, mem, cpu, web_shell_port, container_name, os_name, open_port=None, rand_port=None) -> bool : 
+        
+        shell = "bash"
+
+        if "alpine" in os_name:
+            shell = "sh"
+
+        if open_port == None:
+            port_dict = {web_shell_port+'/tcp':web_shell_port}
+        else:
+            port_dict = {web_shell_port+'/tcp':web_shell_port,
+                rand_port+'/tcp':open_port
+            }
+
+        try:
+
+            self.client.containers.run(
+                image="catone/inspire:%s"%os_name,
+                command="ttyd_linux.x86_64 -p %s %s -x"%(web_shell_port, shell),
+                cpu_period="100000",
+                cpu_quota="%s0000"%cpu,
+                mem_limit="%sm"%mem,
+                name=container_name,
+                ports=port_dict,
+                restart_policy={"Name": "always"}
+                tty=True,
+                detach=True
+                )
+        except:
+            return False
+
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    test = CreateContainer()
+    # test.is_create_container()
---
+++ API/redisCli.py
@@ -0,0 +1,54 @@
+import redis
+
+
+class ConnectRedis(object):
+
+
+
+    def __init__(self):
+
+        redisPool = redis.ConnectionPool(host="127.0.0.1", port=6379, db=0)
+        self.redisCli = redis.StrictRedis(connection_pool=redisPool)
+
+
+    def set_value(self, key :str, value :str) -> bool:
+
+        if key != None or key != '':
+
+            self.redisCli.set(key, value)
+
+            return True
+        else:
+            return False
+
+
+    def get_value(self, key :str) -> str:
+
+        result = self.redisCli.get(key)
+        if result != None:
+            return result.decode()
+        else:
+            return None
+
+    def is_container(self, containerId :str) -> bool:
+
+        result = self.redisCli.get(containerId)
+        if result != None:
+            return True
+        else:
+            return False
+
+    def set_container(self, containerId :str) -> bool:
+
+        result = self.redisCli.get(containerId)
+        if result != None:
+            return True
+        else:
+            return False
+
+
+
+test = ConnectRedis()
+test.set_value("name", "Yuefeng Zhu")
+print(test.get_value("name"))
+
