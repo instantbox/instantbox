4d2d7cc73c7414834dfc2ab94a2924bd30844162
Wei He <git@weispot.com>
2019-02-28 08:49:27 -0500

    style: update python code to comply with pep8
    used `yapf --in-place --recursive .`

--- api/rmContainer.py
+++ api/rmContainer.py
@@ -1,14 +1,12 @@
 import docker
 
+
 class RmContainer(object):
-
-
     def __init__(self):
 
         self.client = docker.from_env()
 
-
-    def is_rm_container(self, container_id) -> bool : 
+    def is_rm_container(self, container_id) -> bool:
         try:
             container = self.client.containers.get(container_id)
         except docker.errors.NotFound:
@@ -19,7 +17,6 @@ class RmContainer(object):
 
 
 if __name__ == '__main__':
-    
 
     test = RmContainer()
     print(test.is_rm_container("redisInstance"))
